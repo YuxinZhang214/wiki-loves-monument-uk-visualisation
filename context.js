ecba00e42369fdc185dde8d3b599f0f3f3606e85
YuxinZhang214 <72717144+YuxinZhang214@users.noreply.github.com>
2025-02-06 17:00:37 +0800

    Skipping SSR for now

--- context.js
+++ context.js
@@ -1,31 +1,95 @@
-'use client';
+'use client'
 
-import React, { createContext, useMemo, useState, useEffect } from 'react';
-import useSWR from 'swr';
+import React, { createContext, useState, useEffect } from 'react';
 
 export const DataContext = createContext(null);
 export const WindowDimensionsContext = createContext(null);
 
 export const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'https://wiki-loves-monument-uk.toolforge.org/api';
 
-const fetcher = (url) => fetch(url).then(res => res.json());
-
 export const DataProvider = ({ children }) => {
-  // 通过 useSWR 获取数据
-  const { data: stats } = useSWR(`${apiUrl}/competition/statistics`, fetcher);
-  const { data: yearly } = useSWR(`${apiUrl}/submissions/yearly`, fetcher);
-  const { data: daily } = useSWR(`${apiUrl}/submissions/daily`, fetcher);
-  const { data: community } = useSWR(`${apiUrl}/participants/submissions`, fetcher);
-  const { data: cumulativeYearly } = useSWR(`${apiUrl}/submissions/yearly/total`, fetcher);
-  const { data: locations } = useSWR(`${apiUrl}/monuments/locations`, fetcher);
-  const { data: heritage_destinations } = useSWR(`${apiUrl}/monuments/destinations`, fetcher);
-  const { data: inceptions } = useSWR(`${apiUrl}/monuments/inceptions`, fetcher);
-  const { data: participants } = useSWR(`${apiUrl}/participants`, fetcher);
-  const { data: monument_images } = useSWR(`${apiUrl}/monuments/images`, fetcher);
-  const { data: submission_images } = useSWR(`${apiUrl}/submissions/images`, fetcher);
 
-  // 避免不必要的渲染
-  const contextValue = useMemo(() => ({
+    // 1. Intro section
+    const [stats, setStats] = useState({});
+    // 2. Contribution Section
+    const [yearly, setYearly] = useState({});
+    const [daily, setDaily] = useState({});
+    const [community, setCommunity] = useState({});
+    // 3. Impact Section
+    const [cumulativeYearly, setCumulativeYearly] = useState({});
+    const [heritage_destinations, setHeritageDestinations] = useState({});
+    const [locations, setLocations] = useState([]);
+    const [inceptions, setInceptions] = useState({});
+    // 4. Participant section
+    const [participants, setParticipants] = useState([]);
+    // 5. monuments
+    const [monument_images, setMonumentImages] = useState([]);
+    // 6. image submissions
+    const [submission_images, setSubmissionImages] = useState([]);
+
+  useEffect(() => {
+
+    console.log(apiUrl)
+   
+    // 1. Fetch competition statistics
+    fetch(`${apiUrl}/competition/statistics`)
+      .then(response => response.json())
+      .then(data => setStats(data));
+
+    // 2.1 Fetch yearly submissions
+    fetch(`${apiUrl}/submissions/yearly`)
+      .then(response => response.json())
+      .then(data => setYearly(data));
+    
+    // 2.2 Fetch daily submissions
+    fetch(`${apiUrl}/submissions/daily`)
+    .then(response => response.json())
+    .then(data => setDaily(data));
+
+    // 2.3 Fetch community data
+    fetch(`${apiUrl}/participants/submissions`)
+      .then(response => response.json())
+      .then(data => setCommunity(data));
+
+    // 3.1 Fetch submission cumulative yearly data
+    fetch(`${apiUrl}/submissions/yearly/total`)
+      .then(response => response.json())
+      .then(data => setCumulativeYearly(data));
+
+    // 3.2 Fetch monument locations & heritage destination data
+    fetch(`${apiUrl}/monuments/locations`)
+       .then(response => response.json())
+       .then(data => setLocations(data));
+
+    // 3.2 Fetch heritage destination data
+    fetch(`${apiUrl}/monuments/destinations`)
+       .then(response => response.json())
+       .then(data => setHeritageDestinations(data));
+
+    // 3.4 Fetch monument inceptions date
+    fetch(`${apiUrl}/monuments/inceptions`)
+      .then(response => response.json())
+      .then(data => setInceptions(data));
+
+    // 4.1 Fetch all the participants
+    fetch(`${apiUrl}/participants`)
+      .then(response => response.json())
+      .then(data => setParticipants(data));
+
+    // 5.1 Fetch all the monuments
+    fetch(`${apiUrl}/monuments/images`)
+      .then(response => response.json())
+      .then(data => setMonumentImages(data));
+
+    // 6.1 Fetch all image submissions
+    fetch(`${apiUrl}/submissions/images`)
+      .then(response => response.json())
+      .then(data => setSubmissionImages(data));
+
+    
+  }, []);
+
+  const contextValue = {
     stats,
     yearly,
     daily,
@@ -37,7 +101,7 @@ export const DataProvider = ({ children }) => {
     participants,
     monument_images,
     submission_images
-  }), [stats, yearly, daily, community, cumulativeYearly, locations, heritage_destinations, inceptions, participants, monument_images, submission_images]);
+  };
 
   return (
     <DataContext.Provider value={contextValue}>
