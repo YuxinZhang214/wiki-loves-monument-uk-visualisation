b1261b9086539324302b6578d4a620fab56db6bd
YuxinZhang214 <72717144+YuxinZhang214@users.noreply.github.com>
2025-02-06 22:36:19 +0800

    Image optimisation for Home page
    using Image component from next.js

--- app/home/ImageCarousal.js
+++ app/home/ImageCarousal.js
@@ -1,18 +1,21 @@
-import React from 'react';
-
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+import Image from 'next/image';
 
 const imageUrls = [
   '/images/image2.jpg',
 ];
 
 const ImageCarousel = () => {
-
   return (
     <div className="flex flex-col items-center justify-center mt-7">
       <div className="w-2/3 aspect-w-1 aspect-h-1">
-        <img src={imageUrls[0]} className="w-full h-full object-cover" />
+        <Image 
+          src={imageUrls[0]} 
+          alt="Carousel Image" 
+          width={1000} // 原始图片宽度
+          height={700} // 原始图片高度
+          layout="responsive" // 响应式布局
+          priority // This ensures the image is loaded as a priority
+        />
       </div>
     </div>
   );
