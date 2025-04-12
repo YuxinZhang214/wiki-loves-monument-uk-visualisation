c96d29684e24c31bad77a7b419498bb741be0154
Yuxin Zhang <72717144+YuxinZhang214@users.noreply.github.com>
2025-04-12 20:12:06 +0800

    Add lighthouse config & ci

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,7 +1,13 @@
 module.exports = {
     ci: {
+      collect: {
+        startServerCommand: 'npm run start',
+        url: ['http://localhost:3000'],
+        startServerReadyPattern: 'started server on',
+        startServerReadyTimeout: 120000,
+      },
       upload: {
         target: 'temporary-public-storage',
       },
     },
-};
+  };
